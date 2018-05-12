77fba023bfe8368f50b5e3b00fa0062a4ea1deaf
Stephen Lau <stephen@edument.se>
2018-05-12 22:22:40 +0200

    Update auth to detect if user is logged in correctly

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,12 @@
 import React from "react";
-import {auth, provider} from '../../firebase';
+import firebase, {auth, provider} from '../../firebase';
 
 export default class Header extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
-      user: null
+      userProfile: null,
+      isSignedIn: false
     };
 
     this.login = this.login.bind(this);
@@ -13,31 +14,38 @@ export default class Header extends React.Component{
   }
 
   logout() {
-
+    auth.signOut();
   }
 
   login() {
     auth.signInWithPopup(provider)
-      .then((result) => {
-        const user = result.user;
-        this.setState({
-          user
-        });
-      });
+  }
+
+  componentDidMount() {
+    // Updating the `isSignedIn` and `userProfile` local state attributes when the Firebase Auth
+    // state changes.
+    this.unregisterAuthObserver = firebase.auth().onAuthStateChanged((user) => {
+      this.setState({ isSignedIn: !!user, userProfile: user });
+    });
+  }
+
+  componentWillUnmount() {
+    // Un-registers the auth state observer.
+    this.unregisterAuthObserver();
   }
 
   render() {
     return <div className="jumbotron">
       <h1 className="display-4">Skåneleden progress tracker</h1>
-      {this.state.user
-        ? <p>Welcome back {this.state.user.displayName} below you can see your progress.  Happy hiking!</p>
+      {this.state.isSignedIn
+        ? <p>Welcome back {this.state.userProfile.displayName} below you can see your progress.  Happy hiking!</p>
         : <p className="lead">
           Here you can track your progress in completing all of the Skåneleden trails. If you wish to save your progress
           you'll need to sign in below!
         </p>
       }
       <p className="lead">
-        {this.state.user
+        {this.state.isSignedIn
           ? <button className="btn btn-primary btn-lg" onClick={this.logout}>Sign out</button>
           : <button className="btn btn-primary btn-lg" onClick={this.login}>Sign in</button>
         }
