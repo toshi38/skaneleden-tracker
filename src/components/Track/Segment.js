8f14aa064334f59da07cdd1e5e7ce7994d8da0bc
Stephen Lau <stephen@edument.se>
2018-05-12 19:28:06 +0200

    Calculate how much of the trail is completed

--- src/components/Track/Segment.js
+++ src/components/Track/Segment.js
@@ -1,25 +1,39 @@
 import React from "react";
 import {removeTrailNumber} from "../../helpers";
 
-export default ({trackSlug, name, slug, lengthKm}) => {
-  const checkId = `${trackSlug}-${slug}`;
-  const segmentName = removeTrailNumber(name);
-  return <div>
-    <div className="form-check">
-      <input type="checkbox" className="form-check-input" id={checkId}/>
-      <label className="form-check-label float-right" htmlFor={checkId}>{Math.round(lengthKm * 10)/10} km</label>
-      <label
-        style={{
-          textOverflow: "ellipsis",
-          overflow: "hidden",
-          whiteSpace: "nowrap",
-          display: "block"
-        }}
-        className="form-check-label"
-        htmlFor={checkId}
-        title={segmentName}>
+export default class Segment extends React.Component {
+  constructor(props) {
+    super(props);
+    this.completed = props.completed;
+  }
+  render () {
+    let {trackSlug, name, slug, lengthKm, updateSegment} = this.props;
+    const checkId = `${trackSlug}-${slug}`;
+    const segmentName = removeTrailNumber(name);
+
+    let toggleSegment = (event) => {
+      this.setState({completed: true});
+      updateSegment(event.target.checked, slug);
+    };
+
+    return <div>
+      <div className="form-check">
+        <input type="checkbox" className="form-check-input" id={checkId} checked={this.completed} onChange={toggleSegment}/>
+        <label className="form-check-label float-right" htmlFor={checkId}>{Math.round(lengthKm * 10)/10} km</label>
+        <label
+          style={{
+            textOverflow: "ellipsis",
+            overflow: "hidden",
+            whiteSpace: "nowrap",
+            display: "block"
+          }}
+          className="form-check-label"
+          htmlFor={checkId}
+          title={segmentName}>
           {segmentName}
-      </label>
+        </label>
+      </div>
     </div>
-  </div>
+  }
+
 }
