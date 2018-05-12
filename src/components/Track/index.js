d04f0ed1e69fd6204b843e3bc998a33c5092fb00
Stephen Lau <stephen@edument.se>
2018-05-12 23:12:53 +0200

    Track changes to the routes..

--- src/components/Track/index.js
+++ src/components/Track/index.js
@@ -1,35 +1,70 @@
 import React from "react";
 import _ from 'lodash';
 import Segment from './Segment';
+import firebase from '../../firebase';
 
 export default class Track extends React.Component{
   constructor(props) {
     super(props);
     this.updateSegment = this.updateSegment.bind(this);
     let segmentsObject = _.keyBy(props.segments, "slug");
+    let segmentsCompleted = props.segments.reduce((acc, val) => ({...acc, [val.slug]: false}), {});
     this.state = {
       ...props,
-      segments: segmentsObject
+      segments: segmentsObject,
+      segmentsCompleted,
+      userProfile: null,
+      isSignedIn: false
     };
+    this.databaseLocationBase = `/progress/${this.state.slug}/`;
+
   }
 
   updateSegment(completed, segmentSlug) {
-    this.setState({
-      segments: {
-        ...this.state.segments,
-        [segmentSlug]: {
-          ...this.state.segments[segmentSlug],
-          completed
+    let segmentsCompleted = {
+      ...this.state.segmentsCompleted,
+      [segmentSlug]: completed
+    };
+    if(this.state.isSignedIn) { //Logged in:
+      firebase.database().ref(this.databaseLocationBase + this.state.userProfile.uid).set(segmentsCompleted)
+    } else {
+      //Can't depend on firebase so update state ourselves:
+      //Normally this is done in the firebase callback.
+      this.setState({segmentsCompleted});
+    }
+  }
+
+  componentDidMount() {
+    // Updating the `someData` local state attribute when the Firebase Realtime Database data
+    // under the '/someData' path changes.
+    this.unregisterAuthObserver = firebase.auth().onAuthStateChanged((user) => {
+      this.setState({ isSignedIn: !!user, userProfile: user });
+      this.firebaseRef = firebase.database().ref(this.databaseLocationBase + this.state.userProfile.uid);
+      this.firebaseCallback = this.firebaseRef.on('value', (snap) => {
+        console.log("Update!", snap.val())
+        if(snap.val()) {
+          this.setState({ segmentsCompleted: snap.val() });
         }
-      }
-    })
+        else {
+          console.log("No state found in database, initializing to current state");
+          firebase.database().ref(this.databaseLocation + this.state.userProfile.uid).set(this.state.segmentsCompleted);
+        }
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    // Un-registers the auth state observer.
+    this.unregisterAuthObserver();
+    // Un-register the listener on '/someData'.
+    this.firebaseRef.off('value', this.firebaseCallback);
   }
 
   render() {
     let {name, slug, segments} = this.state;
     let totalLength = _.reduce(segments, (acc, seg) => acc += seg.lengthKm, 0);
     let completedLength = _.reduce(segments, (acc, seg) => {
-      if(seg.completed) {
+      if(this.state.segmentsCompleted[seg.slug]) {
         return acc += seg.lengthKm;
       }
       return acc;
@@ -43,8 +78,8 @@ export default class Track extends React.Component{
           {
             _.map(segments, segment =>
               <Segment
-                key={segment.slug} trackSlug={slug} name={segment.name} slug={segment.slug} lengthKm={segment.lengthKm}
-                completed={segment.completed} updateSegment={this.updateSegment}
+                key={`${segment.slug}-${this.state.segmentsCompleted[segment.slug]}`} trackSlug={slug} name={segment.name} slug={segment.slug} lengthKm={segment.lengthKm}
+                completed={this.state.segmentsCompleted[segment.slug]} updateSegment={this.updateSegment}
               />)
           }
         </div>
